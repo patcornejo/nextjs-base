aa683ccf2243a258395cfd09f87af2907a9ede1d
Pat Cornejo <pat.cornejo.a@gmail.com>
2022-12-11 15:45:24 -0300

    Base NextJS + TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index: React.FunctionComponent = () => {
+    return (
+        <div>
+            <h1>Index</h1>
+            <p>Soy un index</p>
+        </div>
+    )
+}
+
+export default Index;
